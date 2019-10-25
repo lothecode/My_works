822a6c02a58527aa9d51cca3d15b92959ab296a6
chienfang <lothecode@gmail.com>
2019-10-25 18:24:23 +0800

    first released

---
+++ today-events-wiki/README.md
@@ -0,0 +1,8 @@
+# a simple web application to show what events happened today in Wikipedia
+
+## Features
+- default: user can see what happen today(month/date) in the history, source from wikipedia
+- user can select a date(month/date) to see events happened in the history, source from wikipedia
+
+
+
---
+++ today-events-wiki/index.js
@@ -0,0 +1,54 @@
+const monthNamesEn = [
+  'January', 'February', 'March', 'April', 'May', 'June',
+  'July', 'August', 'September', 'October', 'November', 'December'
+]
+const dateObject = new Date() //Fri Jul 15 2016 16:23:49 GMT+0800 (CST)
+const date = dateObject.getDate()
+const month = dateObject.getMonth()
+const today = `${monthNamesEn[month]}_${date}`
+
+const show = document.querySelector('.results')
+const monthSelc = document.querySelector('#month')
+const dateSelc = document.querySelector('#date')
+const form = document.querySelector('form')
+const resultDate = document.querySelector('#result-date')
+
+function selectForm() {
+  let htmlContent = ''
+  for (i of monthNamesEn) {
+    htmlContent = `
+  <option>${i}</option>
+  `
+    monthSelc.innerHTML += htmlContent
+  }
+  for (let i = 1; i <= 31; i++) {
+    htmlContent =
+      `<option>${i}</option>
+  `
+    dateSelc.innerHTML += htmlContent
+  }
+}
+
+
+function display(target) {
+  axios.get(`https://en.wikipedia.org/w/api.php?origin=*&action=parse&page=${target}&format=json`)
+    .then(function (response) {
+      let content = response.data.parse.text["*"]
+      let sliceIndex = 'Events\">edit</a><span class=\"mw-editsection-bracket\">]</span></span></h2>\n'
+      let start = content.indexOf(sliceIndex)
+      // console.log(a.length)
+      let end = content.indexOf('\n<h2', start)
+      let events = content.slice(start + sliceIndex.length, end)
+      show.innerHTML = events
+      resultDate.innerHTML = target
+      selectForm()
+    })
+    .catch(function (error) { console.log(error); });
+}
+display(today)
+
+form.addEventListener('submit', function (event) {
+  event.preventDefault()
+  let chosen = `${monthSelc.value}_${dateSelc.value}`
+  display(chosen)
+})
